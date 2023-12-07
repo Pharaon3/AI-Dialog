6edf75bde7d695a424ab85118b2ccd933fb4a877
Pharaon3 <Pharaon313@outlook.com>
2023-12-07 11:22:03 +0900

    style fix

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import "react-datepicker/dist/react-datepicker.css";
 function App() {
   const [startDate, setStartDate] = useState(new Date());
   const ExampleCustomInput = forwardRef(({ value, onClick }, ref) => (
-    <button className="example-custom-input" onClick={onClick} ref={ref}>
+    <button className="example-custom-input bg-sky-300 p-2 rounded-full" onClick={onClick} ref={ref}>
       {value}
     </button>
   ));
@@ -37,32 +37,32 @@ function App() {
         <div>
           <div className='page-title'>AI News Web Scraper</div>
         </div>
-        <div className='article shadow-xl max-w-2xl mb-4 flex items-start justify-between'>
+        <div className='article shadow-xl max-w-3xl mb-4 flex items-start justify-between'>
           <div className='flex flex-col items-start justify-between'>
             <div className='article-title'>When will the first general AI system be devised, tested, and publicly announced?</div>
-            <div className='article-content'>This question is a duplicate of this one with a stronger operationalization for artificial general intelligence, and including robotic capabilities. I will copy relevant parts of that question to this one.</div>
+            <div className='article-content'>This question is a duplicate of this one with a stronger operationalization for artificial general intelligence, and including robotic capabilities. I will copy relevant parts of that question to this one....</div>
             <a href='https://www.metaculus.com/questions/5121/date-of-artificial-general-intelligence/' className='article-link' target='_blank'>Read Article</a>
           </div>
           <Button className='bg-transparent hover:bg-sky-100 text-blue-700 font-semibold hover:text-white py-1 px-1 border border-blue-500 hover:border-transparent rounded' onClick={openModal}>⭐</Button>
         </div>
-        <div className='article shadow-xl max-w-2xl mb-4 flex items-start justify-between'>
+        <div className='article shadow-xl max-w-3xl mb-4 flex items-start justify-between'>
           <div className='flex flex-col items-start justify-between'>
             <div className='article-title'>Title 2. Video</div>
             <video className="h-full w-full rounded-lg w-3/4" controls>
               <source src="https://docs.material-tailwind.com/demo.mp4" type="video/mp4" />
               Your browser does not support the video tag.
             </video>
-            <div className='article-content'>This question is a duplicate of this one with a stronger operationalization for artificial general intelligence, and including robotic capabilities. I will copy relevant parts of that question to this one.</div>
+            <div className='article-content'>This question is a duplicate of this one with a stronger operationalization for artificial general intelligence, and including robotic capabilities. I will copy relevant parts of that question to this one.....</div>
             <a href='https://docs.material-tailwind.com/demo.mp4' className='article-link' target='_blank'>Watch Video</a>
           </div>
           <Button className='bg-transparent hover:bg-sky-100 text-blue-700 font-semibold hover:text-white py-1 px-1 border border-blue-500 hover:border-transparent rounded' onClick={openModal}>⭐</Button>
         </div>
-        <div className='article shadow-xl max-w-2xl mb-4 flex items-start justify-between'>
+        <div className='article shadow-xl max-w-3xl mb-4 flex items-start justify-between'>
           <div className='flex flex-col items-start justify-between'>
             <div className='article-title'>Title 3. Tweet</div>
             <div className='flex items-center'>
               <img src="./pic.png" className='w-20 h-20 rounded-full border-2 object-cover'/>
-              <div className='article-content'>This question is a duplicate of this one with a stronger operationalization for artificial general intelligence, and including robotic capabilities. I will copy relevant parts of that question to this one.</div>
+              <div className='article-content'>This question is a duplicate of this one with a stronger operationalization for artificial general intelligence, and including robotic capabilities. I will copy relevant parts of that question to this one.....</div>
             </div>
             <a href='https://www.metaculus.com/questions/5121/date-of-artificial-general-intelligence/' className='article-link' target='_blank'>See Tweet</a>
           </div>
@@ -94,13 +94,19 @@ function App() {
                 leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
               >
                 <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all lg:my-8 lg:w-full lg:max-w-2xl">
-                  <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                  <div className="bg-gray-100 px-4 pb-4 pt-2 sm:p-6 sm:pb-4">
                     <div className="sm:flex sm:items-start">
-                      <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                        <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900 text-xl">
+                      <div className="mt-3 text-center sm:mt-0 sm:text-left">
+                        <Dialog.Title as="h3" className="text-base leading-6 text-gray-900 text-xl font-bold uppercase">
                           Share this Post
                         </Dialog.Title>
-                        <div className="mt-3">
+                      </div>
+                    </div>
+                  </div>
+                  <div className="bg-white px-4 pt-2 sm:p-10 sm:pb-4">
+                    <div className="sm:flex sm:items-start">
+                      <div className="mt-3 text-center sm:mt-0 sm:text-left">
+                        <div className="">
                           <h5 className='flex items-center'>
                             <svg className="h-5 w-5 text-red-500"
                               viewBox="0 0 24 24"
@@ -113,12 +119,12 @@ function App() {
                               <rect x="2" y="9" width="4" height="12" />
                               <circle cx="4" cy="4" r="2" />
                             </svg>
-                            <span className='text-lg pl-1'>Linkedin</span>
+                            <span className='text-lg pl-1 font-semibold'>Linkedin</span>
                           </h5>
                           <div className='flex'>
                             <div className='border flex w-3/4 mr-2 p-2'>
                               <p className="text-sm text-gray-500">
-                                This question is a duplicate of this one with a stronger operationalization for artificial general intelligence, and including robotic capabilities. I will copy relevant parts of that question to this one.
+                                This question is a duplicate of this one with a stronger operationalization for artificial general intelligence, and including robotic capabilities. I will copy relevant parts of that question to this one....
                               </p>
                               <svg className="h-14 w-14 text-red-500 cursor-pointer" viewBox="0 0 24 24" fill="none" stroke="#666" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">  <rect x="9" y="9" width="13" height="13" rx="2" ry="2" />  <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1" /></svg>
                             </div>
@@ -135,14 +141,14 @@ function App() {
                                 stroke="none"
                                 d="M22.99121,3.9502a.99942.99942,0,0,0-1.51074-.85938,7.47956,7.47956,0,0,1-1.873.793,5.15234,5.15234,0,0,0-3.374-1.24219,5.23238,5.23238,0,0,0-5.22363,5.06348A11.03194,11.03194,0,0,1,4.19629,3.78125,1.01154,1.01154,0,0,0,3.33887,3.416a.99852.99852,0,0,0-.78516.5,5.2755,5.2755,0,0,0-.24219,4.76855l-.00195.00195a1.0411,1.0411,0,0,0-.49512.88868,3.04174,3.04174,0,0,0,.02637.43945,5.1854,5.1854,0,0,0,1.56836,3.3125.99813.99813,0,0,0-.06641.76953,5.20436,5.20436,0,0,0,2.36231,2.92187,7.46464,7.46464,0,0,1-3.58985.44825.99975.99975,0,0,0-.665,1.833A12.94248,12.94248,0,0,0,8.46,21.36133,12.7878,12.7878,0,0,0,20.9248,11.998,12.82166,12.82166,0,0,0,21.46,8.35156c0-.06543,0-.13281-.001-.20019A5.76963,5.76963,0,0,0,22.99121,3.9502ZM19.68457,7.16211a.995.995,0,0,0-.2334.70215c.00977.165.00879.331.00879.4873a10.82371,10.82371,0,0,1-.4541,3.08106A10.68457,10.68457,0,0,1,8.46,19.36133a10.93791,10.93791,0,0,1-2.55078-.30078,9.47951,9.47951,0,0,0,2.94238-1.56348A1.00033,1.00033,0,0,0,8.25,15.71094a3.208,3.208,0,0,1-2.21387-.93457q.22413-.04248.44532-.10547a1.00026,1.00026,0,0,0-.08008-1.94336,3.19824,3.19824,0,0,1-2.25-1.72559,5.29929,5.29929,0,0,0,.54492.0459,1.02093,1.02093,0,0,0,.9834-.69629A.9998.9998,0,0,0,5.2793,9.21484,3.19559,3.19559,0,0,1,3.85547,6.542c0-.0664.00195-.13281.00586-.19824a13.01365,13.01365,0,0,0,8.209,3.47949,1.02046,1.02046,0,0,0,.81739-.3584,1.00037,1.00037,0,0,0,.206-.86816,3.15653,3.15653,0,0,1-.08691-.72852A3.23,3.23,0,0,1,16.2334,4.6416a3.18428,3.18428,0,0,1,2.34472,1.02051A.993.993,0,0,0,19.499,5.96a9.27073,9.27073,0,0,0,1.21192-.32226A6.68126,6.68126,0,0,1,19.68457,7.16211Z"></path>
                             </svg>
-                            <span className='text-lg pl-1'>Tweet</span>
+                            <span className='text-lg pl-1 font-semibold'>Tweet</span>
                           </h5>
                           <div className='flex'>
                             <div className='border flex w-3/4 mr-2 p-2'>
                               <div className='flex items-center'>
                                 <img src="./pic.png" className='w-20 h-20 rounded-full border-2 mr-2 object-cover'/>
                                 <p className="text-sm text-gray-500">
-                                  This question is a duplicate of this one with a stronger operationalization for artificial general intelligence, and including robotic capabilities. I will copy relevant parts of that question to this one.
+                                  This question is a duplicate of this one with a stronger operationalization for artificial general intelligence, and including robotic capabilities. I will copy relevant parts of that question to this one....
                                 </p>
                               </div>
                               <svg className="h-14 w-14 text-red-500 cursor-pointer" viewBox="0 0 24 24" fill="none" stroke="#666" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">  <rect x="9" y="9" width="13" height="13" rx="2" ry="2" />  <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1" /></svg>
