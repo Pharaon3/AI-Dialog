70519e03fa4539017eb07a95bea098c626d466ae
Pharaon3 <Pharaon313@outlook.com>
2023-12-12 11:17:33 +0900

    schedule updated

--- backend/schedule.js
+++ backend/schedule.js
@@ -2,9 +2,22 @@ const { exec } = require('child_process');
 const cron = require('node-cron');
 
 // Schedule the task to run every day at 12:00 AM
-let scheduledTask = cron.schedule('0 2 * * *', () => {
+let scheduledTask = cron.schedule('20 2 * * *', () => {
     runTask()
 });
+let scheduledTask1 = cron.schedule('25 2 * * *', () => {
+    runTask1()
+});
+let scheduledTask2 = cron.schedule('30 2 * * *', () => {
+    runTask2()
+});
+let scheduledTask3 = cron.schedule('35 2 * * *', () => {
+    runTask3()
+});
+let scheduledTask4 = cron.schedule('40 2 * * *', () => {
+    runTask4()
+});
+
 
 const runTask = () => {
     exec('python youtubescan.py', (error, stdout, stderr) => {
@@ -14,6 +27,9 @@ const runTask = () => {
         }
         console.log(`Command output: ${stdout}`);
     });
+}
+
+const runTask1 = () => {
     exec('python deepmind.py', (error, stdout, stderr) => {
         if (error) {
             console.error(`Error executing command: ${error}`);
@@ -21,6 +37,8 @@ const runTask = () => {
         }
         console.log(`Command output: ${stdout}`);
     });
+}
+const runTask2 = () => {
     exec('python zapier.py', (error, stdout, stderr) => {
         if (error) {
             console.error(`Error executing command: ${error}`);
@@ -28,6 +46,8 @@ const runTask = () => {
         }
         console.log(`Command output: ${stdout}`);
     });
+}
+const runTask3 = () => {
     exec('python automate.py', (error, stdout, stderr) => {
         if (error) {
             console.error(`Error executing command: ${error}`);
@@ -35,6 +55,8 @@ const runTask = () => {
         }
         console.log(`Command output: ${stdout}`);
     });
+}
+const runTask4 = () => {
     exec('python make1.py', (error, stdout, stderr) => {
         if (error) {
             console.error(`Error executing command: ${error}`);
@@ -59,5 +81,8 @@ const runTask = () => {
         });
     });
 }
-
 scheduledTask.start()
+scheduledTask1.start()
+scheduledTask2.start()
+scheduledTask3.start()
+scheduledTask4.start()
